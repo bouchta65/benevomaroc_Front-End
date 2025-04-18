a2eeeb9ec1f3425ab36c4970a232e33a4518e316
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-18 02:25:13 +0100

    Ajout des routes pour afficher les opportunités et les détails d'une opportunité

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import home from '../views/home.vue';
+import opportunites from '../views/opportunities/show.vue';
+import OpportuniteDetails from '../views/opportunities/details.vue';
 
 const routes = [
   {path: '/',name: 'home',component: home},
+  {path: '/opportunites',name: 'opportunites',component: opportunites},
+  {path: "/opportunites/:id",name: "OpportuniteDetails",component: OpportuniteDetails, props: true,}
 ];
 
 const router = createRouter({

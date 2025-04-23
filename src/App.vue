c09e0b13e8240ab4b7920abbef78d3d6767302c1
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-23 22:06:57 +0100

    utilise useRoute pour afficher Unauthorized si la route contient la meta unauthorized

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
-import Navbar from './components/layout/Header.vue'
-import Footer from './components/layout/Footer.vue'
+import Navbar from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+import Unauthorized from './components/layout/unauthorized.vue'; 
+import { useRoute } from 'vue-router';
+
+const route = useRoute(); 
 </script>
 
 <template>
-  <Navbar/>
-  <router-view />
+  <Navbar />
+  <div>
+    <Unauthorized v-if="route.meta.unauthorized" />
+    <router-view v-else />
+  </div>
   <Footer />
 </template>
-
-

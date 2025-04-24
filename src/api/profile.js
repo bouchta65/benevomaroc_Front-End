16f77ac573bb7e1b459b8f3c108ad156c5bd0972
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-24 10:56:59 +0100

    Ajout des méthodes getProfile, updateUserInfo et updateBenevoleDetails pour gérer les informations du profil et les détails du bénévole via l'API

---
+++ src/api/profile.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const api = 'http://127.0.0.1:8000/api';
+
+export default {
+  getProfile(token) {
+    return axios.get(`${api}/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  },
+  updateUserInfo(data, token) {
+    return axios.put(`${api}/profile/benevole/userInfo`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+  },
+  updateBenevoleDetails(data, token) {
+    return axios.put(`${api}/profile/benevole/details`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+}

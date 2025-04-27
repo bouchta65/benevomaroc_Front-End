9af8be7ac5d34f55d1763b52b3f37b262d0be38f
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-27 01:38:23 +0100

    corriger l'importation d'Axios en remplaçant 'API' par 'api'

--- src/api/opportunite.js
+++ src/api/opportunite.js
@@ -1,28 +1,33 @@
 import axios from 'axios';
 
-const API = 'http://127.0.0.1:8000/api/opportunites'; 
+const api = 'http://127.0.0.1:8000/api/opportunites'; 
 
 export default {
   getAll(page = 1) {
-    return axios.get(`${API}?page=${page}`);
+    return axios.get(`${api}?page=${page}`);
   },
 
   getTop3() {
-    return axios.get(`${API}/Top`);
+    return axios.get(`${api}/Top`);
 },
 
   getById(id) {
-    return axios.get(`${API}/${id}`);
+    return axios.get(`${api}/${id}`);
   },
 
   searchOpportunites(ville, titre, types = [], sort) {
     const params = { ville, titre, types, sort };
-    return axios.get(`${API}/search`, { params });
+    return axios.get(`${api}/search`, { params });
   },
 
   getSimilarOpportunites(id) {
-    return axios.get(`${API}/${id}/similar`);
+    return axios.get(`${api}/${id}/similar`);
   },
+
+
+
+
+
 
   
   

e20bc400521cd6095b5069f479f12ac708f8ddd9
bouchta65 <bouchtamohamed01@gmail.com>
2025-05-04 21:28:11 +0100

    Affichage dynamique du profil selon le rôle de l'utilisateur (admin ou association)

---
+++ src/components/profile.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <component :is="profileComponent"></component>  
+    </div>
+  </template>
+  
+  <script>
+  import { authStore } from "@/stores/auth";
+  import profileAdmin from '@/views/admin/profile/show.vue'
+  import profileAssociation from '@/views/association/profile/show.vue'
+
+  
+  export default {
+    data() {
+      return {
+        profileComponent: null
+      }
+    },
+    mounted() {
+      
+      if (authStore.role === 'admin') {
+        this.profileComponent = profileAdmin;
+      } else if (authStore.role === 'association') {
+        this.profileComponent = profileAssociation;
+      } else {
+        this.profileComponent = null; 
+      }
+    }
+  }
+  </script>
+  
